c32409111fadb17039a43874bbe32798f17c2e50
tuananh3491 <anhnguyen280303@gmail.com>
2023-12-07 18:20:50 +0700

    updatemain

---
+++ js/HomeGame.js
@@ -0,0 +1,4 @@
+function toggleDropdown() {
+    var dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+}
